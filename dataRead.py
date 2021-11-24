eca4067e20557e259eec13a7fe46b210056a3893
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-11-24 13:36:17 -0300

    create dataRead.py

---
+++ dataRead.py
@@ -0,0 +1,47 @@
+from os import read
+
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+def removeParemeter(string):
+    index = string.find("(")
+    return string[:index]
+
+
+
+dataFile = open ("akiyo.txt", "r")
+
+lineBegin = 5
+
+stringList = list()
+
+res = list()
+
+for i in range (0, lineBegin):
+    dataFile.readline()
+
+for i in range (0,20):
+    str = dataFile.readline()
+    stringList.append(str)
+
+for sub in stringList:
+    res.append(sub.split())
+
+print("name", end="")
+print(" "*48, end="")
+print("time(%)       " + "c. seconds    " + "s. seconds    " + "calls         " + "self call     " + "total call")
+print()
+for i in res:
+    print(f"{removeParemeter(i[6]):50}", end='  ')
+    for j in i:
+        if is_number(j):
+            print(f"{j:12}", end='  ')
+    print()
+    
+
+
+dataFile.close()
