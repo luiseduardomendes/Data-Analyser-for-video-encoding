63376cadad57dd8a230422c50c1304cd9ceeda44
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-12-30 18:23:46 -0300

    data reading working

--- regularExpressionsTest.py
+++ regularExpressionsTest.py
@@ -1,10 +1,39 @@
 import re
 
-texto = '''
-  0.02     50.95     0.01        1     0.01     0.01  IntraPrediction::init()
-  0.02     50.96     0.01        1     0.01     0.01  MatrixIntraPrediction::MatrixIntraPrediction()
-  0.02     50.97     0.01        1     0.01     0.03  QuantRDOQ::setFlatScalingList()
-  0.02     50.98     0.01                             void invTransformCbCr<2>()
-  0.02     50.99     0.01                             CABACWriter::cuPaletteSubblockInfo()
-'''
-padrao = re.compile('')
+data = open('akiyo.txt')
+stringList = data.read()
+
+pattern = re.compile(r'([\d]+\.[\d]+)[ ]+([\d]+\.[\d]+)[ ]+([\d]+\.[\d]+|[ ]+)[ ]+([\d]+|[ ]+)[ ]+([\d]+\.[\d]+|[ ]+)[ ]+([\d]+\.[\d]+|[ ]+)[ ]+(.+)')
+check = pattern.findall(stringList)
+
+structBuffer = dict()
+dataList = list()
+
+for i in check:
+    
+    structBuffer['percentage time'] = i[0]
+    structBuffer['cumulative time'] = i[1]
+    structBuffer['self seconds'] = i[2]
+    
+    if i[3] != ' ':
+        structBuffer['calls'] = int(i[3])
+    else:
+        structBuffer['calls'] = -1
+    
+    if i[4] != ' ':
+        structBuffer['self ms/call'] = float(i[4])
+    else:
+        structBuffer['self ms/call'] = -1.00
+    
+    if i[5] != ' ':
+        structBuffer['total ms/call'] = float(i[5])
+    else:
+        structBuffer['total ms/call'] = -1.00
+    
+    structBuffer['name'] = i[6]
+
+    dataList.append(structBuffer.copy())
+
+
+for i in range(0,20):
+    print(f"{dataList[i]['percentage time']:8}{dataList[i]['cumulative time']:8}{dataList[i]['self seconds']:8}{dataList[i]['calls']:12}{dataList[i]['self ms/call']:8}{dataList[i]['total ms/call']:8}\t{dataList[i]['name']}")
