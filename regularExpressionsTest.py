e8927937a275315fda344a2c60b7c599ba5977e5
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-12-30 18:58:59 -0300

    first graphic done

--- regularExpressionsTest.py
+++ regularExpressionsTest.py
@@ -1,10 +1,14 @@
+from os import name
 import re
+from matplotlib import pyplot as plt
 
 data = open('akiyo.txt')
 stringList = data.read()
 
 pattern = re.compile(r'([\d]+\.[\d]+)[ ]+([\d]+\.[\d]+)[ ]+([\d]+\.[\d]+|[ ]+)[ ]+([\d]+|[ ]+)[ ]+([\d]+\.[\d]+|[ ]+)[ ]+([\d]+\.[\d]+|[ ]+)[ ]+(.+)')
 check = pattern.findall(stringList)
+
+namePatternFunction = re.compile(r'.+\(')
 
 structBuffer = dict()
 dataList = list()
@@ -29,11 +33,33 @@ for i in check:
         structBuffer['total ms/call'] = float(i[5])
     else:
         structBuffer['total ms/call'] = -1.00
-    
-    structBuffer['name'] = i[6]
+
+    structBuffer['name'] = i[6]    
 
     dataList.append(structBuffer.copy())
 
 
 for i in range(0,20):
     print(f"{dataList[i]['percentage time']:8}{dataList[i]['cumulative time']:8}{dataList[i]['self seconds']:8}{dataList[i]['calls']:12}{dataList[i]['self ms/call']:8}{dataList[i]['total ms/call']:8}\t{dataList[i]['name']}")
+
+percentageTime = list()
+nameFunctions = list()
+
+for i in range(0,20):
+	percentageTime.append(dataList[i]['percentage time'][:])
+	nameFunctions.append(namePatternFunction.findall(dataList[i]['name'])[0][:])
+
+percentageTime.reverse()
+nameFunctions.reverse()
+
+	
+plt.style.use('ggplot')
+plt.title('Functions')
+plt.xlabel('Percentage time')
+
+plt.tight_layout()
+
+plt.barh(nameFunctions, percentageTime)
+
+plt.show()
+
