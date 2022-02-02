49db7bfadfc20e72e179f1b55313a37f28f44daf
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2022-02-02 20:39:09 -0300

    update

--- testes/main.py
+++ testes/main.py
@@ -1,10 +1,17 @@
+import imp
 from gprof_out_reader import GprofOutReader
 from plotterData import Plotter
+from terminalPlotter import TerminalBarhPlotter
 
 d = GprofOutReader("akiyo.txt")
 
-p = Plotter()
+'''p = Plotter()
 p.insertLists(d.dataListsPerFunctionForPlotter['function'], 'function',
             d.dataListsPerFunctionForPlotter['percentageTime'], 'percentageTime')
 p.setOutputFileName("akiyo", '30', 'all_intra')
-p.plotBarhGraph()
+p.plotBarhGraph()'''
+
+b = TerminalBarhPlotter()
+b.barh( xValues=d.dataListsPerFunctionForPlotter['percentageTime'],
+        yValues=d.dataListsPerFunctionForPlotter['function']) 
+b.show()
---
+++ testes/terminalPlotter.py
@@ -0,0 +1,40 @@
+import os
+
+from matplotlib.cbook import maxdict
+
+class TerminalBarhPlotter:
+    __terminalSize__ = int()
+    __dataYaxis__ = list() # names = string
+    __dataXaxis__ = list() # values = float
+    __color__ = str()
+    __bgColor__ = str()    
+    
+    def __init__(self, color='', bgColor=''):
+        self.__color__ = color
+        self.__bgColor__ = bgColor
+        self.__terminalSize__= os.get_terminal_size()
+        self.__terminalSizeX = self.__terminalSize__.columns
+        self.__terminalSizeY = self.__terminalSize__.lines
+        
+
+    def barh(self, xValues, yValues):
+        #if(type(xValues) == list()):
+        self.__dataXaxis__ = xValues
+        #if(type(yValues) == list()):
+        self.__dataYaxis__ = yValues
+        self.__setSize__()
+
+    def __setSize__(self):
+        maxValue = max(self.__dataXaxis__)
+        self.__proportion__ = (self.__terminalSizeX - 35) / maxValue
+        
+    def show(self):
+        for i in range(0, len(self.__dataYaxis__)):
+            if(i % 2 == 0):
+                print('\033[32:40m', end='')
+            else:
+                print('\033[33:40m', end='')
+            print(f'{self.__dataYaxis__[i]:33}[', end='')
+            print('#'*int(self.__proportion__*self.__dataXaxis__[i]),end='')
+            print(']')
+        print('\033[m', end='')
