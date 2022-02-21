73706fb52353edac2d0de4f719dba3a95548db0d
dudupemendes13@gmail.com <dudupemendes13@gmail.com>
2022-02-21 17:02:38 -0300

    implementation of gprof to csv in execution

--- gprof_executer.py
+++ gprof_executer.py
@@ -1,14 +1,22 @@
 import os
 
 class gprof_executer:
-    def __init__(self, cfg_encoder, cfg_video, video_name, qp):
+    def __init__(self, cfg_encoder: str, cfg_video: str, video_name: str, qp: int, frames: int = -1):
         vtm_dir = "/home/luispmendes/VVCSoftware_VTM/"
 
         cfg_dir = vtm_dir + "cfg/"    
         bin_dir = vtm_dir + "bin/"
         out_dir = vtm_dir + "out/"
 
+        if frames == -1:
+            frames_encoded = ''
+        else:
+            frames_encoded = f' -f {frames} '
+
+        print(f'encoding {video_name} with cfg {cfg_encoder}, {cfg_video} and qp {qp}')
+
         os.system(f'{bin_dir}EncoderAppStatic -c {cfg_dir}{cfg_encoder} \
-            -c {cfg_video} -b {out_dir}{video_name}.bin -q {qp} >> {out_dir}log_{video_name}.txt && gprof EncoderAppStatic gmon.out >> {out_dir}{video_name}.txt')
+            -c {cfg_video} -b {out_dir}videos_bin/{video_name}.bin -q {qp}{frames_encoded}>> {out_dir}/logs_execution/log_{video_name}.txt \
+            && gprof {bin_dir}EncoderAppStatic gmon.out >> {out_dir}/logs_gprof/gprof_log_{video_name}.txt')
 
 
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-from datetime import datetime
 import gprof_out_manipulation as gp
 from terminalPlotter import TerminalBarhPlotter as tbp
 from plotterData import Plotter as plt
@@ -8,8 +6,8 @@ from os import path, listdir, system
 from gprof_executer import gprof_executer as gp_exe
 
 
-cfg_videos_dir = '/home/luispmendes/cfg-files/'
-output_dir = '/home/luispmendes/output/'
+cfg_videos_dir = '/home/luispmendes/VVCSoftware_VTM/cfg-files/'
+
 video_cfg = [f for f in listdir(cfg_videos_dir) if path.isfile(path.join(cfg_videos_dir, f)) and f[-4:] == '.cfg']
 
 quant_param = [22,27,32,37]
@@ -19,12 +17,18 @@ enc_cfgs = [
     'encoder_randomaccess_vtm.cfg'
 ]
 
-gp_exe(enc_cfgs[0], video_cfg[0], output_dir, video_cfg[0][:-4], quant_param[3])
+gp_exe(enc_cfgs[1], cfg_videos_dir + video_cfg[2], video_cfg[2][:-4], quant_param[3], 10)
+
+gp_to_csv = gp.GprofToCSV(path_input='/home/luispmendes/VVCSoftware_VTM/out/logs_gprof/gprof_log_'+video_cfg[2][:-4]+'.txt')
+gp_to_csv.read_gprof_out()
+gp_to_csv.convert_data_frame_into_CSV()
+
+
 
 '''
-for video in video_cfg:
-    for qp in quant_param:
-        for cfg in enc_cfgs:
+or video in video_cfg:
+    or qp in quant_param:
+        or cfg in enc_cfgs:
             exec = gp_exe(cfg, cfg_videos_dir+video, output_dir, video[:-4], qp)
             # gp_to_csv = gp.GprofToCSV()
             # gp_to_csv.initialize_path(file_path=f'{video}_{qp}_{cfg}_{datetime.today}')
