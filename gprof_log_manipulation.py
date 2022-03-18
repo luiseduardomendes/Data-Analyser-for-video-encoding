81b20a7418aee0c007095284ec2e1797271ba75e
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2022-03-18 15:59:15 -0300

    create gprof log manipulation

--- gprof_log_manipulation.py
+++ gprof_log_manipulation.py
@@ -1,7 +1,9 @@
 import re
 import pandas as pd
+
 class gprofLogManip():
     def __init__(self, datafile: str) -> None:
+        # verify if 
         if self.__verifyFile__(datafile):
             with open(datafile) as f:
                 self._data_ = f.read()
@@ -10,6 +12,7 @@ class gprofLogManip():
         _data_ = re.findall(r'^\s+(\d+)\s+a\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+$', self._data_, flags=re.M)
 
         data_dict = {
+            'fileName' : str,
             'frames' : int,
             'bitrate' : float,
             'Y_PSNR' : float,
@@ -21,10 +24,13 @@ class gprofLogManip():
         if len(_data_) == 1:
             log = _data_[0]
             for i, key in enumerate(data_dict.keys()):
-                try:
-                    data_dict[key] = int(log[i])
-                except:
-                    data_dict[key] = float(log[i])
+                if key == 'fileName':
+                    data_dict[key] = datafile
+                else:
+                    try:
+                        data_dict[key] = int(log[i-1])
+                    except:
+                        data_dict[key] = float(log[i-1])
             self.dataDict = data_dict
             
         elif len(_data_) == 0:
@@ -47,11 +53,30 @@ class gprofLogManip():
         except:
             raise Exception("data dict has not been set yet")
 
-    def returnDataFrame(self) -> pd.DataFrame:
-        try:
-            return pd.DataFrame(self.data_dict)
-        except:
-            raise Exception("data dict has not been set yet")
 
 class gprofLogSets:
-    pass
+    keys = ('fileName', 'frames', 'bitrate', 'Y_PSNR', 'U_PSNR', 'V_PSNR', 'YUV_PSNR')
+
+    # initialize the dict
+    def __init__(self) -> None:
+        self.__data_dict = {}
+
+        for key in self.keys:
+            self.__data_dict[key] = []
+    
+    # insert a 
+    def appendDict(self, data_dict : dict):
+        for key in self.keys:
+            try:
+                self.__data_dict[key].append(data_dict[key])
+            except:
+                raise KeyError("dict insert is invalid")
+
+
+    # returns the data in a pandas data frame
+    def returnDataFrame(self) -> pd.DataFrame:
+        return pd.DataFrame(self.__data_dict)
+
+    # outputFileName parameter must be the name without the file extension (.xlsx, .txt)
+    def createExcel(self, outputFileName : str):
+        pd.DataFrame(self.__data_dict).to_excel(excel_writer=f'{outputFileName}.xlsx')
