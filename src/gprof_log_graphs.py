68882ec71290cce281430ce4da8f4cd079070850
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2022-03-20 10:51:30 -0300

    update graphs

---
+++ src/gprof_log_graphs.py
@@ -0,0 +1,115 @@
+from logging import raiseExceptions
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import src.gprof_log_manipulation as gplog
+
+
+mnemonics = {
+    'qp' : 'Quantization Parameter',
+    'bitrate' : 'Bitrate',
+    'Y_PSNR' : 'Y_PSNR', 
+    'U_PSNR' : 'U_PSNR', 
+    'V_PSNR' : 'V_PSNR', 
+    'YUV_PSNR' : 'YUV_PSNR'
+}
+
+
+def createGraph(x_list, y_list, videos, outputFileName : str, title : str = '', xlabel : str = '' , ylabel : str = ''):
+    x_axis = []
+    y_axis = []
+
+    try:
+        for x in x_list:
+            x_axis.append(np.array(x))
+        for y in y_list:
+            y_axis.append(np.array(y))
+        
+        if len(x_axis) != len(y_axis) or len(x_axis) != len(videos):
+            raise Exception("number of y and x axis are not the same")
+
+        for i in range(len(x_axis)):
+            if len(x_axis[i]) != len(y_axis[i]):
+                raise Exception("size of lists are not the same")
+
+    except:
+        raise Exception("Objects for axis are not valid")
+    
+    for i in range(len(x_axis)):
+        t = True
+        k = len(x_axis[i]) - 1
+        while t == True:
+            t = False
+            for j in range(k):
+                if x_axis[i][j] > x_axis[i][j+1]:
+                    b = x_axis[i][j]
+                    c = y_axis[i][j]
+                    x_axis[i][j] = x_axis[i][j+1]
+                    y_axis[i][j] = y_axis[i][j+1]
+                    x_axis[i][j+1] = b
+                    y_axis[i][j+1] = c
+                    t = True
+            k -= 1
+
+
+    fig, ax = plt.subplots()
+    
+    for i in range(len(x_axis)):
+        ax.scatter(x_axis[i], y_axis[i], label=videos[i])
+        ax.plot(x_axis[i], y_axis[i])
+        
+
+    ax.set(title=title, xlabel=xlabel, ylabel=ylabel)
+    ax.legend()
+    ax.grid()
+    fig.savefig(outputFileName+'.png')
+    
+    
+
+
+def comparisonSets(set1 : gplog.gprofLogSets, set2 : gplog.gprofLogSets, parameters : tuple):
+    d = [set1.returnDataFrame().to_dict('list'), set2.returnDataFrame().to_dict('list')]
+
+    if len(parameters) != 2 or any(type(p) != str for p in parameters):
+        raise Exception('the parameters must be a tuple with 2 comparison parameters')
+
+    x_param, y_param = parameters
+    
+    fig, ax = plt.subplots()
+
+    for d1 in d:
+
+        x_axis = d1[x_param]
+        y_axis = d1[y_param]
+
+        t = True
+        k = len(x_axis) - 1
+        while t == True:
+            t = False
+            for j in range(k):
+                if x_axis[j] > x_axis[j+1]:
+                    b = x_axis[j]
+                    c = y_axis[j]
+                    x_axis[j] = x_axis[j+1]
+                    y_axis[j] = y_axis[j+1]
+                    x_axis[j+1] = b
+                    y_axis[j+1] = c
+                    t = True
+            k -= 1
+
+
+        x_axis = np.array(x_axis)
+        y_axis = np.array(y_axis)
+    
+        ax.scatter(x_axis, y_axis, label=f'{d1["fileName"][0]}')
+        ax.plot(x_axis, y_axis)
+
+
+    ax.set(title=f'{mnemonics[y_param]} by {mnemonics[x_param]}', xlabel=mnemonics[x_param], ylabel=mnemonics[y_param])
+    ax.legend()
+    ax.grid()
+    
+    
+    fig.savefig(f'{d[0]["fileName"][0]}_{d[1]["fileName"][0]}_{x_param}_{y_param}.png')
+
+
--- src/gprof_log_manipulation.py
+++ src/gprof_log_manipulation.py
@@ -2,59 +2,51 @@ import re
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
+from pprint import pprint
 
 class gprofLogManip():
     def __init__(self, datafile: str) -> None:
         # verify if 
-        if self.__verifyFile__(datafile):
+        if len(re.findall(r'(.+)(?=(?:\.txt$)|(?:\.gplog$))', datafile, flags=re.M)) > 0:
             with open(datafile) as f:
                 self._data_ = f.read()
                 f.close()
 
-        _data_ = re.findall(r'^\s+(\d+)\s+a\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+$', self._data_, flags=re.M)
+            _data_ = re.findall(r'^\s+(\d+)\s+a\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+$', self._data_, flags=re.M)
 
-        data_dict = {
-            'fileName' : str,
-            'qp' : int,
-            'frames' : int,
-            'bitrate' : float,
-            'Y_PSNR' : float,
-            'U_PSNR' : float,
-            'V_PSNR' : float,
-            'YUV_PSNR' : float
-        }
+            data_dict = {
+                'fileName' : str,
+                'qp' : int,
+                'frames' : int,
+                'bitrate' : float,
+                'Y_PSNR' : float,
+                'U_PSNR' : float,
+                'V_PSNR' : float,
+                'YUV_PSNR' : float
+            }
 
-        if len(_data_) == 1:
-            log = _data_[0]
-            for i, key in enumerate(data_dict.keys()):
-                if key == 'fileName':
-                    data_dict[key] = re.findall(r'AI/(.+)_QP\d{2}.txt', datafile)[0]
-                elif key == 'qp':
-                    data_dict[key] = int(re.findall(r'AI/.+_QP(\d{2}).txt', datafile)[0])
-                else:
-                    try:
-                        data_dict[key] = int(log[i-2])
-                    except:
-                        data_dict[key] = float(log[i-2])
-            self.dataDict = data_dict
+            if len(_data_) == 1:
+                log = _data_[0]
+                for i, key in enumerate(data_dict.keys()):
+                    if key == 'fileName':
+                        data_dict[key] = re.findall(r'AI/(.+)_QP\d{2}.txt', datafile)[0]
+                    elif key == 'qp':
+                        data_dict[key] = int(re.findall(r'AI/.+_QP(\d{2}).txt', datafile)[0])
+                    else:
+                        try:
+                            data_dict[key] = int(log[i-2])
+                        except:
+                            data_dict[key] = float(log[i-2])
+                self.dataDict = data_dict
+                
+            elif len(_data_) == 0:
+                self.dataDict = data_dict
             
-        elif len(_data_) == 0:
-            self.dataDict = data_dict
-            return data_dict
+            else:
+                raise Exception("File has more than one execution log")
+        else: 
+            raise Exception('File is not a .txt or a .gplog')
         
-        else:
-            raise Exception("File has more than one execution log")
-
-
-
-    def __verifyFile__(self, filename) -> bool:
-        if len(re.findall(r'(.+)(?!(?:\.txt$)|(?:\.gplog$))', filename)) > 0:
-            return True
-        else:
-            return False
-    
-    
     
     def returnDict(self) -> dict:
         try:
@@ -68,11 +60,18 @@ class gprofLogSets:
     keys = ('fileName', 'qp', 'frames', 'bitrate', 'Y_PSNR', 'U_PSNR', 'V_PSNR', 'YUV_PSNR')
 
     # initialize the dict
-    def __init__(self) -> None:
-        self.__data_dict = {}
+    def __init__(self, datafile : str = 'default') -> None:
+        if datafile == 'default':
+            self.__data_dict = {}
 
-        for key in self.keys:
-            self.__data_dict[key] = []
+            for key in self.keys:
+                self.__data_dict[key] = []
+        elif len(re.findall(r'(.+)(?:\.csv$)', datafile)) > 0:
+            self.__data_dict = pd.read_csv(datafile).to_dict('list')
+        
+        elif len(re.findall(r'(.+)(?:\.xlsx$)', datafile)) > 0:
+            self.__data_dict = pd.read_excel(datafile, index_col=[0]).to_dict('list')
+            
     
     # insert a dict
     def appendDict(self, data_dict : dict):
@@ -94,63 +93,8 @@ class gprofLogSets:
     def createCsv(self, outputFileName : str):
         pd.DataFrame(self.__data_dict).to_csv(path_or_buf=outputFileName+'.csv', index=False, line_terminator='\n')
 
+    def show(self):
+        print(pd.DataFrame(self.__data_dict))
+
     # TODO: implement the method createGraph to a GprofLogSet()
-    
-def createGraph(x_list, y_list, videos, outputFileName : str, title : str = '', xlabel : str = '' , ylabel : str = ''):
-    x_axis = []
-    y_axis = []
-
-    try:
-        for x in x_list:
-            x_axis.append(np.array(x))
-        for y in y_list:
-            y_axis.append(np.array(y))
-        
-        if len(x_axis) != len(y_axis) or len(x_axis) != len(videos):
-            raise Exception("number of y and x axis are not the same")
-
-        for i in range(len(x_axis)):
-            if len(x_axis[i]) != len(y_axis[i]):
-                raise Exception("size of lists are not the same")
-
-    except:
-        raise Exception("Objects for axis are not valid")
-    
-    for i in range(len(x_axis)):
-        t = True
-        k = len(x_axis[i]) - 1
-        while t == True:
-            t = False
-            for j in range(k):
-                if x_axis[i][j] > x_axis[i][j+1]:
-                    b = x_axis[i][j]
-                    c = y_axis[i][j]
-                    x_axis[i][j] = x_axis[i][j+1]
-                    y_axis[i][j] = y_axis[i][j+1]
-                    x_axis[i][j+1] = b
-                    y_axis[i][j+1] = c
-                    t = True
-            k -= 1
-
-            
-                
-
-            
-
-
-    fig, ax = plt.subplots()
-    
-    for i in range(len(x_axis)):
-        ax.scatter(x_axis[i], y_axis[i], label=videos[i])
-        ax.plot(x_axis[i], y_axis[i], label=videos[i])
-        
-
-    ax.set(title=title, xlabel=xlabel, ylabel=ylabel)
-    ax.legend()
-    ax.grid()
-    fig.savefig(outputFileName+'.png')
-    
-    
-
-
     
