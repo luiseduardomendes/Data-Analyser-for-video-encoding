09d362f4279e6a4abe25f1f5870ca07f2f3fc5ee
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2022-04-26 19:19:19 -0300

    update

--- testefsubs.py
+++ testefsubs.py
@@ -1,13 +1,23 @@
-import fileSubstitution as fs
 import os
+import re
+from fileSubstitution import fileSubs
+from pprint import pprint
+from src.gprof_executer import gprof_executer as gpexe
 
-satd_src = 'FilesForVVC/'
+vtm_dir = '/home/luispmendes/VVCSoftware_VTM/'
+satd_src = '/home/luispmendes/Data-Analyser-for-video-encoding/FilesForVVC/'
+satd_dir = vtm_dir + 'source/Lib/CommonLib/'
+cfg_dir = vtm_dir + 'cfg-files/'
+enc_cfg = 'encoder_lowdelay_vtm.cfg'
+video_cfg = cfg_dir + 'RaceHorses.cfg'
 
-satd = []
+pattern = re.compile(r'^/home/luispmendes/Data-Analyser-for-video-encoding/FilesForVVC/(.+)\.cpp$')
+
+satd_settings = []
 for (dirpath, dirnames, filenames) in os.walk(satd_src):
-    satd += [os.path.join(dirpath, file) for file in filenames]
+    satd_settings += [os.path.join(dirpath, file) for file in filenames]
 
-for f in satd:
-    fs.fileSubs(f, 'testeFsubs/')
-    input()
-    
+for settings in satd_settings:
+    fileSubs(settings, satd_dir)
+    print(f"cmake {vtm_dir} -DCMAKE_BUILD_TYPE=Release")
+    print("make")
