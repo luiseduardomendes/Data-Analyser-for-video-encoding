8681c341e6b868ff70dba146e86b77b4e8b21b27
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2022-06-02 13:42:01 -0300

    update

--- gprof_execution.py
+++ gprof_execution.py
@@ -1,5 +1,13 @@
 import os
 import re
+import pandas as pd
+import source.fileSubstitution as gpfs
+
+# create a pandas series to generalize the paths used
+with pd.read_csv('server_paths.csv') as temp:
+    sv_path = temp['path']
+    sv_path = sv_path.set_axis(temp['dir'])
+
 
 pattern = re.compile(r'^FilesForVVC/(.+)$')
 
@@ -15,5 +23,12 @@ with open(exec_buffer, 'r') as f:
 
 os.system('rm {}'.format(exec_buffer))
 os.system('mv {} {}'.format('./temp'+exec_buffer, './'+exec_buffer))
+
+gpfs.fileSubs(file, sv_path["vtm"]+"source/Lib/CommonLib/")
+
+os.system(f'cd {sv_path["vtm"]+"build"}')
+os.system('cmake .. -DCMAKE_BUILD_TYPE=Release')
+os.system('make')
+os.system(f'cd {sv_path["repo"]}')
 
 os.system('python3 main.py "{}"'.format(file))
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import source.gprof_out_manipulation as gp
 import sys
-import os
-from os import path, listdir, system
+from os import path, listdir
 from source.gprof_executer import gprof_executer as gp_exe
-from source.fileSubstitution import fileSubs
+import pandas as pd
 
-cfg_videos_dir = '/home/luispmendes/VVCSoftware_VTM/cfg-files/'
-satd_src = '/home/luispmendes/Data-Analyser-for-video-encoding/FilesForVVCNEW/'
-satd_dir = '/home/luispmendes/VVCSoftware_VTM/source/Lib/CommonLib/'
-out_videos_dir = '/home/luispmendes/VVCSoftware_VTM/out/'
+# create a pandas series to generalize the paths used
+with pd.read_csv('server_paths.csv') as temp:
+    sv_path = temp['path']
+    sv_path = sv_path.set_axis(temp['dir'])
+
+cfg_videos_dir = sv_path['vtm'] + '/cfg-files/'
+satd_src = sv_path['repo'] + '/FilesForVVC/'
+satd_dir = sv_path['commonlib']
+out_videos_dir = sv_path['out']
 
 video_cfg = [f for f in listdir(cfg_videos_dir) if path.isfile(path.join(cfg_videos_dir, f)) and f[-4:] == '.cfg']
 
